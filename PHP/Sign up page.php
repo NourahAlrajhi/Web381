32f68c4c7109055a0baf6c374ba917ec92aa0dfd
maahhaa <99130795+maahhaa@users.noreply.github.com>
2022-05-18 02:17:55 +0300

    deleted

--- PHP/Sign up page.php
+++ PHP/Sign up page.php
@@ -12,13 +12,6 @@ $Email = "";
 $Gend = "";
 $errors = array();
 
-if(isset($_POST['Reg'])) {
-    $Fname = $_POST['Fname'];
-    $Lname = $_POST['Lname'];
-    $Pnum = $_POST['Pnum'];
-    $Email = $_POST['Email'];
-    $Gend = $_POST['Gend'];
- }
 //sign up
 if (isset($_POST['Reg'])) {
     // receive all input values from the form
